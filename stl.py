9df772ec0df6796f912b8d577bc84a9fe32bc613
freshremix <90133873+freshremix@users.noreply.github.com>
2022-09-29 16:41:52 +0330

    Update stl.py

--- stl.py
+++ stl.py
@@ -1,4 +1,1 @@
-#!/bin/sh
-wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
-chmod +x shadowsocksR.sh
-./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+pip install requirements.txt
